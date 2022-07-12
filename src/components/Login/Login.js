a0f3ae804165cefcfe6424dd0b7b1a7650990c32
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-07-12 18:27:34 +0530

    + implement a debouncing + implement a clean up function for timer

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -12,9 +12,22 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    //debouncing for check form validation status
+    const identifier = setTimeout(() => {
+      console.log("typing");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // return a clean up function
+    // this will run as a clen up process before useEffect execute it next time
+    // and before the component is removed from DOM
+    // it will not run first execution but after all execution
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
