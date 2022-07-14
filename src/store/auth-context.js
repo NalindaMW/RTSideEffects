777621cc1b6dc5b840227f507be9e80d9d89344b
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-07-15 00:32:01 +0530

    + Use AuthContextProvider to manage all auth related info in one place + Code clean - App.js

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,39 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AuthContext = createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const value = {
+    isLoggedIn: isLoggedIn,
+    onLogin: loginHandler,
+    onLogout: logoutHandler,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 
 export default AuthContext;
